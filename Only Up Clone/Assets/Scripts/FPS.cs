cb1d93240857f70143d8fb7da9e23396abcada5e
Juzer Baatwala <eternalflame07@protonmail.com>
2024-06-14 18:54:00 +0530

    Initial Commit First

---
+++ Only Up Clone/Assets/Scripts/FPS.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FpsDisplay : MonoBehaviour
+{
+    [SerializeField] private Text fpsText;
+    private float deltaTime;
+
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text = "FPS: " + Mathf.Ceil(fps).ToString();
+    }
+}
